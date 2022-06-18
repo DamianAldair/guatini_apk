ce16a274738a4436b7d6c89f5f57200c71c9ff09
Damian Aldair <daldairgp@gmail.com>
2022-06-18 16:07:47 -0400

    Preferencias y check db

--- main.dart
+++ main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:guatini/global/appdata.dart' as app_data;
+import 'package:guatini/global/tools.dart' as tools;
 import 'package:guatini/pages/main_page.dart';
+import 'package:guatini/pages/selectdb_page.dart';
+import 'package:guatini/providers/sharedpreferences_provider.dart';
 
-void main() {
+void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  UserPreferences prefs = UserPreferences();
+  await prefs.initPrefs();
   runApp(const MyApp());
   FlutterNativeSplash.remove();
 }
@@ -15,10 +20,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: app_data.productName,
-      home: MainPage(),
+      home: tools.checkDB(),
+      routes: {
+        '/main': (BuildContext context) => const MainPage(),
+        '/selectdb': (BuildContext context) => const SelectDbPage(),
+      },
     );
   }
 }
