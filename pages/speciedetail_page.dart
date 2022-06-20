7fa0906a80b71ed09a300dfa67ff08795e362888
Damian Aldair <daldairgp@gmail.com>
2022-06-19 22:49:52 -0400

    Correcto mostrado de lam info básica

--- pages/speciedetail_page.dart
+++ pages/speciedetail_page.dart
@@ -1,8 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:guatini/global/tools.dart' as tools;
+import 'package:guatini/models/specie_model.dart';
+import 'package:guatini/providers/db_provider.dart';
+import 'package:guatini/providers/sharedpreferences_provider.dart';
 import 'package:guatini/widgets/infocards_widget.dart';
 
 class SpecieDetailPage extends StatelessWidget {
-  const SpecieDetailPage({Key? key}) : super(key: key);
+  final int specieId;
+
+  const SpecieDetailPage(this.specieId, {Key? key}) : super(key: key);
 
   final String pageTitle = 'Detalles de la especie';
 
@@ -17,43 +25,87 @@ class SpecieDetailPage extends StatelessWidget {
   }
 
   Widget _myBody() {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(15.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: const FadeInImage(
-                placeholder: AssetImage('assets/images/loading.gif'),
-                placeholderFit: BoxFit.cover,
-                image: AssetImage('assets/images/image_not_available.png'),
-                fit: BoxFit.cover,
-                fadeInDuration: Duration(milliseconds: 100),
-                fadeOutDuration: Duration(milliseconds: 100),
+    return FutureBuilder(
+      future: DBProvider.instance.getSpecie(1),
+      builder: (BuildContext context, AsyncSnapshot<SpecieModel?> snapshot) {
+        if (snapshot.hasData) {
+          final specie = snapshot.data;
+          try {
+            return SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(15.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15.0),
+                      child: FadeInImage(
+                        placeholder:
+                            const AssetImage('assets/images/loading.gif'),
+                        placeholderFit: BoxFit.cover,
+                        image: _getMainImage(specie),
+                        fit: BoxFit.cover,
+                        fadeInDuration: const Duration(milliseconds: 100),
+                        fadeOutDuration: const Duration(milliseconds: 100),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    specie!.commonNamesAsString,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Text('${specie.scientificName}'),
+                  const SizedBox(height: 10.0),
+                  InfoCard(field: 'Dominio', instance: specie.taxdomain),
+                  InfoCard(field: 'Reino', instance: specie.taxkindom),
+                  InfoCard(field: 'Filo', instance: specie.taxphylum),
+                  InfoCard(field: 'Clase', instance: specie.taxclass),
+                  InfoCard(field: 'Orden', instance: specie.taxorder),
+                  InfoCard(field: 'Familia', instance: specie.taxfamily),
+                  InfoCard(field: 'Gérero', instance: specie.taxgenus),
+                  ConservationStateCard(status: specie.conservationStatus),
+                  InfoCard(field: 'Endemismo', instance: specie.endemism),
+                  InfoCard(field: 'Abundancia', instance: specie.abundance),
+                  InfoCard.extended(
+                      field: 'Actividad', instances: specie.activities),
+                  InfoCard.extended(
+                      field: 'Hábitat', instances: specie.habitats),
+                  InfoCard.extended(field: 'Dieta', instances: specie.diets),
+                  const SoundCard(),
+                  Description(description: '${specie.description}'),
+                  const Gallery(),
+                  const SizedBox(height: 10.0),
+                ],
               ),
-            ),
-          ),
-          const Text(
-            'Nombre común',
-            style: TextStyle(
-              fontSize: 20.0,
-            ),
-          ),
-          const SizedBox(height: 10.0),
-          const Text('Nombre científico'),
-          const SizedBox(height: 10.0),
-          const InfoCard(field: 'Dominio', instance: 'd'),
-          const InfoCard(field: 'Reino', instance: 'r'),
-          const InfoCard(field: 'Filo', instance: 'f'),
-          const InfoCard(field: 'Clase', instance: 'c'),
-          const InfoCard(field: 'Orden', instance: 'o'),
-          const InfoCard(field: 'Familia', instance: 'f'),
-          const InfoCard(field: 'Gérero', instance: 'g'),
-          const ConservationStateCard(),
-          const SizedBox(height: 10.0),
-        ],
+            );
+          } catch (e) {
+            return _dbErrorScreen;
+          }
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  Widget get _dbErrorScreen {
+    return const Center(
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Text(tools.warningDB),
       ),
     );
+  }
+
+  _getMainImage(SpecieModel? specie) {
+    final _mediaModel = specie!.mainImage;
+    final _prefs = UserPreferences();
+    final _dbPath = _prefs.dbPath;
+    return _mediaModel == null
+        ? const AssetImage('assets/images/image_not_available.png')
+        : FileImage(File('$_dbPath${_mediaModel.path}'));
   }
 }
