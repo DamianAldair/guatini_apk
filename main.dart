d16235f3477218aa3511e531a88b31e6ad367a46
Damian Aldair <daldairgp@gmail.com>
2022-06-19 22:48:50 -0400

    quitar rutas del main

--- main.dart
+++ main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:guatini/global/appdata.dart' as app_data;
 import 'package:guatini/global/tools.dart' as tools;
-import 'package:guatini/pages/main_page.dart';
-import 'package:guatini/pages/selectdb_page.dart';
 import 'package:guatini/providers/sharedpreferences_provider.dart';
 
 void main() async {
@@ -24,10 +22,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: app_data.productName,
       home: tools.checkDB(),
-      routes: {
-        '/main': (BuildContext context) => const MainPage(),
-        '/selectdb': (BuildContext context) => const SelectDbPage(),
-      },
     );
   }
 }
